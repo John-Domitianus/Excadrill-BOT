493facda02b160502d64cf9bbe1935f9f53eb9a2
John-Domitianus <john.domiciano0207@gmail.com>
2026-04-04 15:28:19 -0300

    Create connect.js

---
+++ database/connect.js
@@ -0,0 +1,7 @@
+﻿const mongoose = require("mongoose");
+
+module.exports = () => {
+    mongoose.connect(process.env.MONGO_URL)
+        .then(() => console.log("✅ Mongo conectado"))
+        .catch(err => console.log("❌ Erro Mongo:", err));
+};
