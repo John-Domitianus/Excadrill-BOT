e5a6e1f2d55ec25a387291547d8872297eacf793
John-Domitianus <john.domiciano0207@gmail.com>
2026-04-04 16:55:23 -0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,4 +1,9 @@
 const { carregarDadosMongo } = require("../services/dataManager");
+const fs = require("fs");
+
+console.log("DIR:", __dirname);
+console.log("FILES:", fs.readdirSync(__dirname));
+console.log("SERVICES EXISTS:", fs.existsSync(__dirname + "/../services"));
 
 module.exports = async (client) => {
     client.on("ready", async () => {
