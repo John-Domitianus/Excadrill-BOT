3e993dd7443b76859e87192d284187d3ffa84955
John-Domitianus <john.domiciano0207@gmail.com>
2026-04-04 17:37:44 -0300

    Update embeds.js

--- utils/embeds.js
+++ utils/embeds.js
@@ -1,1 +1,15 @@
-﻿module.exports = {};
+﻿const { EmbedBuilder } = require("discord.js");
+
+function embedSucesso(msg) {
+    return new EmbedBuilder().setColor(0x57F287).setDescription(`✅ ${msg}`);
+}
+
+function embedErro(msg) {
+    return new EmbedBuilder().setColor(0xED4245).setDescription(`❌ ${msg}`);
+}
+
+function embedInfo(msg) {
+    return new EmbedBuilder().setColor(0x5865F2).setDescription(`ℹ️ ${msg}`);
+}
+
+module.exports = { embedSucesso, embedErro, embedInfo };
